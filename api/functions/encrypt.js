b690e02beb4d446f02920fd14a818fa0b7467b17
akshaykeenath <akshaykeenath@gmail.com>
2023-10-22 15:19:08 +0530

    hashing deploy issues

--- api/functions/encrypt.js
+++ api/functions/encrypt.js
@@ -1,14 +1,24 @@
 const bcrypt = require("bcrypt");
 
 async function hashPassword(password) {
-  const saltRounds = 10;
-  const hashedPassword = await bcrypt.hash(password, saltRounds);
-  return hashedPassword;
+  try {
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    return hashedPassword;
+  } catch (error) {
+    console.error("Error in hashPassword:", error);
+    throw error;
+  }
 }
 
 async function verifyPassword(password, hashedPassword) {
-  const isMatch = await bcrypt.compare(password, hashedPassword);
-  return isMatch;
+  try {
+    const isMatch = await bcrypt.compare(password, hashedPassword);
+    return isMatch;
+  } catch (error) {
+    console.error("Error in verifyPassword:", error);
+    throw error;
+  }
 }
 
 module.exports = {
