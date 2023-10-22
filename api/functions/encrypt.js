6f8d025c405f9c799e0a37ee9664a8c449941e46
akshaykeenath <akshaykeenath@gmail.com>
2023-10-22 14:29:30 +0530

    bcrypt and session added

---
+++ api/functions/encrypt.js
@@ -0,0 +1,17 @@
+const bcrypt = require("bcrypt");
+
+async function hashPassword(password) {
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+  return hashedPassword;
+}
+
+async function verifyPassword(password, hashedPassword) {
+  const isMatch = await bcrypt.compare(password, hashedPassword);
+  return isMatch;
+}
+
+module.exports = {
+  hashPassword,
+  verifyPassword,
+};
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,8 +1,8 @@
 const express = require("express");
-
 const router = express.Router();
 const userModel = require("../models/userModel");
 const mongoose = require("mongoose");
+const { hashPassword, verifyPassword } = require("../functions/encrypt");
 
 router.post("/login", (req, res) => {
   const user = {
@@ -10,16 +10,15 @@ router.post("/login", (req, res) => {
     pass: req.body.pass,
   };
   userModel
-    .findOne({ uname: user.uname, password: user.pass })
+    .findOne({ uname: user.uname })
     .then((existingUser) => {
-      if (existingUser) {
-        // User is authenticated, generate a token or session here
+      if (existingUser && verifyPassword(user.pass, existingUser.password)) {
+        // User is authenticated
         req.session.user = existingUser;
         res.status(200).json({
           message: "User authenticated",
           session: req.sessionID,
           user: req.session.user,
-          // Add your token or session information here
         });
       } else {
         res.status(401).json({
@@ -50,11 +49,25 @@ router.get("/logout", (req, res) => {
     });
   } else {
     res.status(401).json({
-      error: "no session available",
+      error: "No session available",
     });
   }
 });
-router.post("/register", (req, res) => {
+
+router.get("/session", (req, res) => {
+  const session = req.sessionID;
+  if (session) {
+    res.status(200).json({
+      message: session,
+    });
+  } else {
+    res.status(401).json({
+      error: "session not available",
+    });
+  }
+});
+
+router.post("/register", async (req, res) => {
   const user = {
     fname: req.body.fname,
     lname: req.body.lname,
@@ -64,51 +77,43 @@ router.post("/register", (req, res) => {
     email: req.body.email,
   };
 
-  // Check if a user with the same email or uname already exists
-  userModel
-    .findOne({
+  try {
+    const existingUser = await userModel.findOne({
       $or: [
         { uname: user.uname },
         { email: user.email },
         { mobile: user.mobile },
       ],
-    })
-    .then((existingUser) => {
-      if (existingUser) {
-        res.status(400).json({
-          message:
-            "User already exists with the same email or username or mobile.",
-        });
-      } else {
-        const newUser = new userModel({
-          _id: new mongoose.Types.ObjectId(),
-          fname: user.fname,
-          lname: user.lname,
-          uname: user.uname,
-          mobile: user.mobile,
-          email: user.email,
-          password: user.pass,
-        });
-        newUser
-          .save()
-          .then((result) => {
-            res.status(200).json({
-              message: "User added",
-            });
-          })
-          .catch((err) => {
-            res.status(500).json({
-              error: err,
-            });
-          });
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        error: err,
-      });
     });
+
+    if (existingUser) {
+      res.status(400).json({
+        message:
+          "User already exists with the same email or username or mobile.",
+      });
+    } else {
+      const hashedPassword = await hashPassword(user.pass);
+      const newUser = new userModel({
+        _id: new mongoose.Types.ObjectId(),
+        fname: user.fname,
+        lname: user.lname,
+        uname: user.uname,
+        mobile: user.mobile,
+        email: user.email,
+        password: hashedPassword, // Store the hashed password in the database
+      });
+
+      await newUser.save();
+      res.status(200).json({
+        message: "User added",
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      error: err,
+    });
+  }
 });
 
 module.exports = router;
